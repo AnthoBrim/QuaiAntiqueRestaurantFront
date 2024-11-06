ec38150d6f14145505058347990684242482c280
AnthoBrim <antho.robin.pro@gmail.com>
2024-11-06 13:31:46 +0100

    Test

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route";
+
+//définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    
+];
+
+export const websiteName = "Quai Antique";
