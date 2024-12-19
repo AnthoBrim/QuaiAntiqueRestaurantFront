4b50df8fbe83dae35d1f788ccb5d1bcce91e392a
AnthoBrim <antho.robin.pro@gmail.com>
2024-12-19 10:07:59 +0100

    Ajout de la navigation

--- Readme.md
+++ Readme.md
@@ -1,4 +1,3 @@
 # QuaiAntique
 
 Site Web test
-git config --global user.name "AnthoBrim" git config --global user.email " antho.robin.pro@gmail.com"  
--- Router/allRoutes.js
+++
@@ -1,9 +0,0 @@
-import Route from "./Route.js";
-
-//définir ici vos routes
-export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    
-];
-
-export const websiteName = "Quai Antique";
